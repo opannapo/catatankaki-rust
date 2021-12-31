e8ec5d190f31dab91adc40ec4347e2094252b9ab
opannapo <taufan.alfazri@gmail.com>
2021-12-31 19:04:31 +0700

    Create Module Loop & arguments module number parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
 
+mod module;
+
+
 const ARG_HELP: &str = "help";
+const ARG_LOOP: &str = "loop";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Error :: no args! Format :  cargo run <module> <number>");
+        eprintln!("Error :: no args! Format ->  cargo run <module> <number>");
         return;
     }
-
 
     parsing_args(args);
 }
@@ -20,14 +23,44 @@ fn parsing_args(args: Vec<String>) {
         ARG_HELP => {
             println!("----------- Service Modules Name ----------");
             println!("help");
+            println!("loop , available number [1]");
             println!("----------- --------- ----------");
             println!("Example :  cargo run loop 1");
             println!("Example :  cargo run param 2");
             println!("----------- --------- ----------");
             return;
         }
-        _ => {
-            println!("Unknown Arguments")
+        ARG_LOOP => {
+            let (ok, number) = checking_module_number(args);
+            if ok {
+                match number {
+                    1 => {
+                        module::perulangan::loop1(1, 5);
+                    }
+                    _ => {
+                        eprintln!("Unknown Module Number");
+                    }
+                }
+            }
+            return;
         }
+        _ => {
+            eprintln!("Unknown Arguments");
+        }
+    }
+
+    fn checking_module_number(args: Vec<String>) -> (bool, i32) {
+        if args.len() < 3 {
+            eprintln!("Unknown Module Number");
+            return (false, -1);
+        }
+
+        let result = args[2].parse::<i32>().unwrap_or(-1);
+        if result == -1 {
+            eprintln!("Invalid Module Number");
+            return (false, -1);
+        }
+
+        return (true, result);
     }
 }
---
+++ src/module/perulangan.rs
@@ -0,0 +1,6 @@
+pub fn loop1(start: i32, end: i32) {
+    println!("Example for loop with parameter start | end");
+    for i in start..end {
+        println!("loop for {}", i);
+    }
+}
