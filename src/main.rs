09f6ade2e15f4b5dc8d46c04d5fadf8f27867deb
opannapo <taufan.alfazri@gmail.com>
2021-12-31 18:23:16 +0700

    Main cli parsing arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+
+const ARG_HELP: &str = "help";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Error :: no args! Format :  cargo run <module> <number>");
+        return;
+    }
+
+
+    parsing_args(args);
+}
+
+fn parsing_args(args: Vec<String>) {
+    let name = &args[1];
+
+    match name.as_str() {
+        ARG_HELP => {
+            println!("----------- Service Modules Name ----------");
+            println!("help");
+            println!("----------- --------- ----------");
+            println!("Example :  cargo run loop 1");
+            println!("Example :  cargo run param 2");
+            println!("----------- --------- ----------");
+            return;
+        }
+        _ => {
+            println!("Unknown Arguments")
+        }
+    }
 }
