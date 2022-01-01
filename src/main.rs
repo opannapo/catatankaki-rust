28eac5ceb0dd02509740932ac9e53587ed530102
opannapo <taufan.alfazri@gmail.com>
2022-01-01 22:48:43 +0700

    Rapih rapih

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 mod module;
+mod entity;
 
 
 const ARG_HELP: &str = "help";
@@ -23,73 +24,85 @@ fn parsing_args(args: Vec<String>) {
 
     match name.as_str() {
         ARG_HELP => {
-            println!("----------- Service Modules Name ----------");
-            println!("help");
-            println!("loop , available number [1]");
-            println!("----------- --------- ----------");
-            println!("Example :  cargo run loop 1");
-            println!("Example :  cargo run param 2");
-            println!("----------- --------- ----------");
-            return;
+            action_module_help();
         }
         ARG_LOOP => {
-            let (ok, number) = checking_module_number(args);
-            if ok {
-                match number {
-                    1 => module::perulangan::loop1(1, 5),
-                    2 => module::perulangan::loop2(),
-                    3 => module::perulangan::loop3(),
-                    _ => {
-                        eprintln!("Unknown Module Number");
-                    }
-                }
-            }
-            return;
+            action_module_loop(args);
         }
         ARG_TUPLES => {
-            let (ok, number) = checking_module_number(args);
-            if ok {
-                match number {
-                    1 => module::tuples::tuples1(),
-                    2 => module::tuples::tuples2(),
-                    _ => {
-                        eprintln!("Unknown Module Number");
-                    }
-                }
-            }
-            return;
+            action_module_tuples(args);
         }
         ARG_CLOSURES => {
-            let (ok, number) = checking_module_number(args);
-            if ok {
-                match number {
-                    1 => module::closures::closures1(),
-                    2 => module::closures::closures2(),
-                    3 => module::closures::closures3(),
-                    _ => {
-                        eprintln!("Unknown Module Number");
-                    }
-                }
-            }
-            return;
+            action_module_closures(args);
         }
         _ => {
             eprintln!("Unknown Arguments");
         }
     }
+}
 
-    fn checking_module_number(args: Vec<String>) -> (bool, i32) {
-        if args.len() < 3 {
-            eprintln!("Unknown Module Number");
-            return (false, -1);
+fn checking_module_number(args: Vec<String>) -> (bool, i32) {
+    if args.len() < 3 {
+        eprintln!("Unknown Module Number");
+        return (false, -1);
+    }
+
+    let result = args[2].parse::<i32>().unwrap_or(-1);
+    if result == -1 {
+        eprintln!("Invalid Module Number");
+        return (false, -1);
+    }
+
+    return (true, result);
+}
+
+fn action_module_help() {
+    println!("----------- Service Modules Name ----------");
+    println!("help");
+    println!("loop , available number [1]");
+    println!("----------- --------- ----------");
+    println!("Example :  cargo run loop 1");
+    println!("Example :  cargo run param 2");
+    println!("----------- --------- ----------");
+}
+
+fn action_module_loop(args: Vec<String>) {
+    let (ok, number) = checking_module_number(args);
+    if ok {
+        match number {
+            1 => module::perulangan::loop1(1, 5),
+            2 => module::perulangan::loop2(),
+            3 => module::perulangan::loop3(),
+            _ => {
+                eprintln!("Unknown Module Number");
+            }
         }
+    }
+}
 
-        let result = args[2].parse::<i32>().unwrap_or(-1);
-        if result == -1 {
-            eprintln!("Invalid Module Number");
-            return (false, -1);
+fn action_module_tuples(args: Vec<String>) {
+    let (ok, number) = checking_module_number(args);
+    if ok {
+        match number {
+            1 => module::tuples::tuples1(),
+            2 => module::tuples::tuples2(),
+            _ => {
+                eprintln!("Unknown Module Number");
+            }
         }
+    }
+}
 
-        return (true, result);
+fn action_module_closures(args: Vec<String>) {
+    let (ok, number) = checking_module_number(args);
+    if ok {
+        match number {
+            1 => module::closures::closures1(),
+            2 => module::closures::closures2(),
+            3 => module::closures::closures3(),
+            _ => {
+                eprintln!("Unknown Module Number");
+            }
+        }
     }
 }
